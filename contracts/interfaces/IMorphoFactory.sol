e035de1dbb425cc3f261d3af89d92aeba286cecf
RobertMCForster <rforster510@yahoo.com>
2025-11-13 17:43:01 -0800

    Update solc version

--- contracts/interfaces/IMorphoFactory.sol
+++ contracts/interfaces/IMorphoFactory.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.8.26;
+pragma solidity ^0.8.26;
 
 interface IMorphoFactory {
     function createMarket(address loanToken, address collateralToken, address oracle, address irm, uint256 lltv) external returns (address);
